5524e26499f89b71bef411f778252e88f0cafcbf
dinhkhai0201 <nguyendinhkha2111997@gmail.com>
2024-02-13 22:36:33 +0700

    font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,16 @@ export default function Document() {
         <meta name="description" content="My Valentine" />
         <meta name="keywords" content="My Valentine" />
         <meta name="author" content="@dinhkhai0201" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin='anonymous'
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
