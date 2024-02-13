08cfd9b7ce627416e1a507dcd57730f167706ad5
dinhkhai0201 <nguyendinhkha2111997@gmail.com>
2024-02-13 22:01:10 +0700

    change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <title>My Valentine</title>
+        <meta name="description" content="My Valentine" />
+        <meta name="keywords" content="My Valentine" />
+        <meta name="author" content="@dinhkhai0201" />
       </Head>
       <body>
         <Main />
