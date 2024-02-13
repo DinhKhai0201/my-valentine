6e9f9d95dff1d97c38bb3a73a08413fe930be93e
dinhkhai0201 <nguyendinhkha2111997@gmail.com>
2024-02-13 20:44:20 +0700

    change

---
+++ src/pages/Button.tsx
@@ -0,0 +1,22 @@
+
+interface IButton {
+    children: JSX.Element
+    onClick?: ()=> void;
+    onMouseOver?: ()=> void;
+    props?: any;
+}
+
+function Button({
+    children,
+    onClick = () => {},
+    onMouseOver = () => {},
+    ...props
+}: IButton) {
+  return (
+    <button className="  text-lg cursor-pointer bg-[#ffe3e1] text-[#e67373] shadow-[1px_1px_2px_#ff9494] transition-[0.3s] mb-5 m-2.5 px-5 py-2.5 rounded-xl  hover:bg-[#ff9494] hover:text-[#ffe3e1] hover:shadow-[1px_1px_2px_#ffe3e1] hover:border-2 hover:border-solid hover:border-[#ff9494] border-[2px] border-solid border-[#ff9494]" onClick={onClick} onMouseOver={onMouseOver} {...props}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,251 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import dayjs from "dayjs";
+import { useRef, useState } from "react";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
+import DateTimePicker from "react-datetime-picker";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import Button from "./Button";
+import useSound from 'use-sound'; 
 
-const inter = Inter({ subsets: ["latin"] });
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+const STEPS = {
+  CONFIRM: 1,
+  DATE: 2,
+  FOOD: 3,
+  AFTER: 4,
+  OK: 5,
+};
+
+const FOODS = [
+  {
+    title: "Chicken",
+    src: "garan.jpeg",
+  },
+  {
+    title: "Hot noddle",
+    src: "mycay.jpeg",
+  },
+  {
+    title: "Korean food",
+    src: "koreanfood.jpeg",
+  },
+  {
+    title: "Sushi",
+    src: "sushi.jpeg",
+  },
+];
+const AFTERS = [
+  {
+    title: "Park",
+    src: "park.jpeg",
+  },
+  {
+    title: "Beach",
+    src: "beach.jpeg",
+  },
+  {
+    title: "Coffee",
+    src: "coffee.webp",
+  },
+  {
+    title: "Draw",
+    src: "draw.jpeg",
+  },
+];
+
+function App() {
+  const [continueSound] = useSound("continue.mp3");
+  const [x, setx] = useState(52);
+  const [y, sety] = useState(55);
+  const form = useRef();
+
+  const [step, setStep] = useState<number>(STEPS.CONFIRM);
+  const [value, onChange] = useState<Value>(new Date());
+  const [food, setFood] = useState<string>("");
+  const [after, setAfter] = useState<string>("");
+
+  function mouseOver() {
+    setx(Math.random() * 100);
+    sety(Math.random() * 100);
+  }
+
+  let noStyle = {
+    left: x + "%",
+    top: y + "%",
+    position: "absolute",
+  };
+
+  /* code for email alert sent when she says yes */
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs
+  //     .sendForm("11111", "template_w4y121f", form.current, "KI7bceeNiZsp0c9Kp")
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  //   e.target.reset();
+  // };
+  const onSelectFood = (foodName: string) => {
+    setFood(foodName);
+  };
+  const onSelectActivity = (activityName: string) => {
+    setAfter(activityName);
+  };
+
+  switch (step) {
+    case STEPS.CONFIRM: {
+      return (
+        <div className="py-10">
+          <h1 className="text-center font-bold text-[38px] text-[#de324e]">
+            Will you be my Valentine? 🌹
+          </h1>
+          <div className="flex items-center justify-center">
+            <img className="w-[200px]" src="capy.gif" alt="capy" />
+          </div>
+          <div className="flex items-center justify-center">
+            <Button onClick={() => setStep(STEPS.DATE)}>
+              <p className="font-bold">YES!</p>
+            </Button>
+            {/* <form onSubmit={sendEmail} ref={form}>
+          <button 
+            style={yesStyle}
+            type="submit"
+            onClick={clickedYes}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            YES!
+          </button> 
+        </form> */}
+            <Button onMouseOver={mouseOver} style={noStyle}>
+              <p className="font-bold">no</p>
+            </Button>
+          </div>
         </div>
-      </div>
+      );
+    }
+    case STEPS.DATE: {
+      return (
+        <div className="py-10 h-[100vh] bg-[#95bfa7]">
+          <h1 className="text-center font-bold text-[38px] text-[#d5de95]">
+            Which date can we meet? 🌹
+          </h1>
+          <div className="flex items-center justify-center">
+            <DateTimePicker onChange={onChange} value={value} />
+          </div>
+          <div className="flex items-center justify-center">
+            <Button onClick={() => setStep(STEPS.FOOD)}>
+              <p className="font-bold">Continue</p>
+            </Button>
+          </div>
+        </div>
+      );
+    }
+    case STEPS.FOOD: {
+      return (
+        <div className="py-10 h-[100vh] bg-[#d5de95]">
+          <h1 className="text-center font-bold text-[38px] text-[black]">
+            Which type of food do u like? 🌹
+          </h1>
+          <div className="flex items-center justify-center ">
+            <div className="grid grid-cols-2  max-w-xs gap-10">
+              {FOODS.map((item, index) => {
+                const selected = item.title === food;
+                return (
+                  <div
+                    className={`w-full cursor-pointer ${
+                      selected
+                        ? "rounded bg-[#ff9494]  border-[3px] border-solid border-[#ff9494]"
+                        : ""
+                    }`}
+                    key={index}
+                    onClick={() => onSelectFood(item.title)}
+                  >
+                    <img className="w-full" src={item.src} alt={item.title} />
+                    <p className="text-center">{item.title}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <Button  onClick={() => food &&  setStep(STEPS.AFTER)}>
+              <p className="font-bold">Continue</p>
+            </Button>
+          </div>
+        </div>
+      );
+    }
+    case STEPS.AFTER: {
+      return (
+        <div className="py-10 h-[100vh] bg-[#c095cf]">
+          <h1 className="text-center font-bold text-[38px] text-[#f5d6ba]">
+            What will we do after? 🌹
+          </h1>
+          <div className="flex items-center justify-center ">
+            <div className="grid grid-cols-2  max-w-xs gap-10">
+              {AFTERS.map((item, index) => {
+                  const selected = item.title === after;
+                return (
+                  <div
+                  className={`w-full cursor-pointer ${
+                    selected
+                      ? "rounded bg-[#ff9494]  border-[3px] border-solid border-[#ff9494]"
+                      : ""
+                  }`}
+                    key={index}
+                    onClick={() => onSelectActivity(item.title)}
+                  >
+                    <img className="w-full" src={item.src} alt={item.title} />
+                    <p className="text-center">{item.title}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <Button onClick={() => setStep(STEPS.OK)}>
+              <p className="font-bold">Continue</p>
+            </Button>
+          </div>
+        </div>
+      );
+    }
+    case STEPS.OK: {
+      return (
+        <div className="py-10 h-[100vh] bg-[#e2a1e6]">
+          <h1 className="text-center font-bold text-[38px] text-[red]">
+            Thank for be my Valentine 🌹
+          </h1>
+          <div className="flex items-center justify-center mt-[10px]">
+            <div className="max-w-xs ">
+              <p className="text-[28px] text-center">
+                So at{" "}
+                {dayjs(value as unknown as string).format("HH:mm DD/MM/YYYY")}.
+                We are having {food}, then we will go to {after}
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <Button onClick={()=>continueSound && continueSound()}>
+              <p className="font-bold">Happy</p>
+            </Button>
+          </div>
+        </div>
+      );
+    }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    default:
+      break;
+  }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  return null;
 }
+
+export default App;
